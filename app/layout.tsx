a652579e98007beaf35f3a01da244e95f0cc8526
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-29 17:31:18 +0200

    created navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
+import  Navbar  from '@/components/Navbar'
 
+// Fonts
 const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ['latin'], 
   weight: ['300', '400', '500', '600', '700']
  })
 
+// Change the title and description of the web page
 export const metadata: Metadata = {
-  title: 'Pricewise',
-  description: 'Track product prices effortlessly and save money on your online shopping.',
+  title: 'Smartprice',
+  description: 'Track amazon product prices effortlessly and save money on your online shopping.',
 }
 
 export default function RootLayout({
@@ -22,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="max-w-10xl mx-auto">
+          <Navbar />
           {children}
         </main>
       </body>
