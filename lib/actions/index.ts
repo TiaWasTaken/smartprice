613b63cd8a31becbe0a16153057758befea73869
TiaWasTaken <arganetto.mattia@gmail.com>
2024-10-29 22:57:33 +0100

    created the database

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string){
@@ -8,8 +9,18 @@ export async function scrapeAndStoreProduct(productUrl: string){
   }
 
   try {
+
+    connectToDB();
+
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
+    if(!scrapedProduct){
+      return;
+    }
+
+    let product = scrapedProduct;
+
+    // const existingProduct = await 
 
   } catch(error: any){
     throw new Error(`Failed to create/update product: ${error.message}`);
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
-import { extractCurrency, extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string){
   if(!url){
@@ -55,23 +55,29 @@ export async function scrapeAmazonProduct(url: string){
 
     const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
 
+    const description = extractDescription($);
+
     // Construct the data object
     const data = {
       url,
       currency: currency || '€',
       image: imageUrls[0],
       title,
-      currentPrice: Number(currentPrice),
-      originalPrice: Number(originalPrice),
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
       priceHistory: [],
       discountRate: Number(discountRate),
       category: 'category',
       reviewsCount: 100,
       stars: 4.6,
       isOutOfStock: isOutOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averege: Number(currentPrice) || Number(originalPrice)
     }
 
-    console.log(data);
+    return data;
   } catch (error: any) {
     throw new Error(`Failed to scrape the product: ${error.message}`);
   }
