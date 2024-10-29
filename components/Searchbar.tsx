020234201ffeae0200b8e0d4ebf1b457b5c6ebfd
TiaWasTaken <arganetto.mattia@gmail.com>
2024-10-29 10:35:56 +0100

    created the isLoading state and disabled the buttons

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,64 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React, { FormEvent, useState } from 'react';
 
-const handleSubmit = () => {}
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    // Check if hostname contains amazon.com or amazon.it...
+    if (hostname.includes('amazon.com') || hostname.includes('amazon.') || hostname.endsWith('amazon')) {
+      return true;
+    }
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+
+  return false;
+};
 
 const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if(!isValidLink){
+      return alert('Please provide a valid Amazon Link.');
+    }
+
+    try {
+      setIsLoading(true);
+
+      // Scrape our first product
+
+    } catch(err){
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+
+  };
+
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-     <input type="text" placeholder="Enter Amazon Product Link" className="searchbar-input"/> 
-      <button type="submit" className="searchbar-btn">Search</button>
+      <input
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter Amazon Product Link"
+        className="searchbar-input"
+      />
+      <button type="submit" disabled={searchPrompt === ''} className="searchbar-btn">{isLoading ? 'Searching...' : 'Search'}</button>
     </form>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
+
